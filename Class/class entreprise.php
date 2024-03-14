2781be4c7d6e84e0649f9fb739881bee37c86807
romain1021 <150360051+romain1021@users.noreply.github.com>
2024-03-14 09:53:07 +0100

    class Entreprise

---
+++ Class/class entreprise.php
@@ -0,0 +1,20 @@
+<?php 
+
+Class Entreprise{
+    private $id;
+    private $nom;
+    private $prenom;
+    private $numClient;
+    private $societe;
+    private $poste;
+    private $email;
+    private $idCommercial;
+    private $dateCreationCompte;
+
+
+}
+
+
+
+
+?>
