bfca9e2db4d8920461761b15a1c70e55225d0350
Noah-SIO <laplaze.noah@gmail.com>
2024-03-15 09:23:34 +0100

    modif utilisateur

--- Class/test_Utilisateur.php
+++ Class/test_Utilisateur.php
@@ -1,5 +1,6 @@
 <?php 
 require_once('utilisateur.php');
 $bdsqll = new PDO("mysql:host=localhost;dbname=mediatheque", 'root', '');
-$test = new ManagerMedias($bdsqll);
+$test = new ManagerUtilisateur($bdsqll);
+$test->SearchUserByType("non");
 ?>
