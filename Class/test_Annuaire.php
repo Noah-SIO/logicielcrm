bba4ff33de6f09d40579f57e57e9f3a99bd13a79
Noah-SIO <laplaze.noah@gmail.com>
2024-03-15 10:54:20 +0100

    test annuaire modif

--- Class/test_Annuaire.php
+++ Class/test_Annuaire.php
@@ -3,3 +3,23 @@ require_once('Annuaire.php');
 $bdsqll = new PDO("mysql:host=localhost;dbname=crm", 'root', '');
 $test = new ManagerAnnuaire($bdsqll);
 ?>
+<html>
+    <form method="post">
+            <label for="recherche"> Entré une Recherche : </label></br>
+            <input type="text" name="recherche"></br>
+            <!--<label for="type"> Entré un Type de valeurs(nom,prenom,identifiants) : </label></br>
+            <input type="text" name="type"></br>-->
+            <select name="choosetype" id="selecttype">
+            <option value="">--Please choose an option--</option>
+            <option value="type">Type</option>
+            <option value="valeur de contact">Valeur de Contact</option>
+            </select>
+            <input type="submit" name="rechercher" id='rechercher' value='Rechercher'>  </br></br>
+            
+    </form>
+</html>
+<?php
+if (isset($_POST['recherche'])&&isset($_POST['choosetype'])){
+    $test->SearchAnnuaireByType($_POST['recherche'],$_POST['choosetype']);
+    echo"true";//test
+}
