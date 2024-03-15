6f92389d39fa632f779ebaa48ac72b6f18f0672f
Noah-SIO <laplaze.noah@gmail.com>
2024-03-15 09:27:02 +0100

    modif et ajout partie romaric avec noah

--- Class/test_Utilisateur.php
+++ Class/test_Utilisateur.php
@@ -3,4 +3,25 @@ require_once('utilisateur.php');
 $bdsqll = new PDO("mysql:host=localhost;dbname=mediatheque", 'root', '');
 $test = new ManagerUtilisateur($bdsqll);
 $test->SearchUserByType("nonoa");
+///Romaric
+$test = new Utilisateur("Martin", "Robert", "Rmartin", 1, "martin71", "martin@test.fr", 0606060606);
+//$nom = $test -> getNom();
+//echo $nom;
+?>
+<html>
+    <form method="post">
+            <label for="login">identifiant</label></br>
+            <input type="text" name="login" id="login"></br>
+            <label for="password"> Mot de passe</label></br>
+            <input type="password" name="password"></br>
+            <input type="submit" name="connection" id='connection'>  </br></br>
+    </form>
+</html>
+<?php
+
+$testManager = new ManagerUtilisateur();
+if (isset($_POST['login'])){
+    $testManager -> verifIdentifiant($_POST['login']);
+}
+
 ?>
