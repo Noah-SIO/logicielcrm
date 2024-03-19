689e75c7178c92a5e4d95678675feede3f758626
ciraam <150357561+ciraam@users.noreply.github.com>
2024-03-19 08:55:13 +0100

    Add files via upload

--- Class/test_Assistance.php
+++ Class/test_Assistance.php
@@ -19,6 +19,12 @@ require_once('assistance.php');
             <input type="number" name="nombre3" id="nombre3" placeholder="entrer un chiffre"></br>
             <input type="submit" name="rechercher" id='rechercher' value='Rechercher'></br>
     </form>
+    <form method="post">
+            <label for="statut"> Pour changer le statut</label></br>
+            <input type="number" name="statut" id="statut" placeholder="entrer le statut (1,2,3)"></br>
+            <input type="number" name="idStatut" id="idStatut" placeholder="entrer l'id"></br>
+            <input type="submit" name="rechercher" id='rechercher' value='Rechercher'></br>
+    </form>
 </html>
 <?php
 
@@ -33,6 +39,10 @@ if (isset($_POST['nombre2'])){
 if (isset($_POST['nombre3'])){
     $testManager = new ManagerAssistance();
     $testManager -> getUnsolvedIssues($_POST['nombre3']);
+}
+if (isset($_POST['statut']) && isset($_POST['idStatut'])){
+    $testManager = new ManagerAssistance();
+    $testManager -> updateStatut($_POST['idStatut'], $_POST['statut']);
 }
 
 ?>
