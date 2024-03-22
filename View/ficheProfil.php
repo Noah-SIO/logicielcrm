605e016975a4affff9039cae2be1b4d25e75b2e7
Yanisouspopo <tonperepointfr@gmail.com>
2024-03-22 09:48:51 +0100

    FicheProfil.php (en cours)

--- View/ficheProfil.php
+++ View/ficheProfil.php
@@ -1,7 +1,23 @@
 <?php
-include 'Class/utilisateur.php';
+include '../Class/utilisateur.php';
 $managerUtilisateur = new ManagerUtilisateur();
+$utilisateur = null;
 if (isset($_SESSION['login']) && isset($_SESSION['mdp'])) {
     $login = $_SESSION['login'];
     $mdp = $_SESSION['mdp'];
+    if ($managerUtilisateur->checkLoginInfos($login, $mdp)) {
+        $utilisateur = $managerUtilisateur->GetUser($login);
+    }
+}
+?>
+</head>
+    <h1>Profil de <?= $utilisateur['prenom'] . ' ' . $utilisateur['nom']; ?></h1>
+    <p><strong>Identifiant:</strong> <?= $utilisateur['identifiant']; ?></p>
+    <p><strong>Profil:</strong> <?= $managerUtilisateur->getProfilById($utilisateur['profil']); ?></p>
+    <p><strong>Email:</strong> <?= $utilisateur['email']; ?></p>
+    <p><strong>Numéro de téléphone:</strong> <?= $utilisateur['numTel']; ?></p>
+</html>
 
+
+
+# A MODIFIER (EN COURS)
