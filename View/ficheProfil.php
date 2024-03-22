29b7ee7cc35d1c85a560144c55277f4c07838d6a
Yanisouspopo <tonperepointfr@gmail.com>
2024-03-22 09:38:21 +0100

    Ficheprofil.PHP

--- View/ficheProfil.php
+++ View/ficheProfil.php
@@ -5,22 +5,3 @@ if (isset($_SESSION['login']) && isset($_SESSION['mdp'])) {
     $login = $_SESSION['login'];
     $mdp = $_SESSION['mdp'];
 
-    if ($managerUtilisateur->checkLoginInfos($login, $mdp)) {
-        $utilisateur = $managerUtilisateur->GetUser($login);
-
-        if ($utilisateur) {
-            echo "<h1>Profil de {$utilisateur['prenom']} {$utilisateur['nom']}</h1>";
-            echo "<p><strong>Identifiant:</strong> {$utilisateur['identifiant']}</p>";
-            echo "<p><strong>Profil:</strong> {$managerUtilisateur->getProfilById($utilisateur['profil'])}</p>";
-            echo "<p><strong>Email:</strong> {$utilisateur['email']}</p>";
-            echo "<p><strong>Numéro de téléphone:</strong> {$utilisateur['numTel']}</p>";
-        } else {
-            echo "<p>Impossible de récupérer les informations du profil.</p>";
-        }
-    } else {
-        echo "<p>Identifiant ou mot de passe incorrect.</p>";
-    }
-} else {
-    echo "<p>Vous n'êtes pas connecté.</p>";
-}
-?>
