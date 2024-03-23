84807757bd2c2452d6d2b2f09515ef35385e027b
ciraam <150357561+ciraam@users.noreply.github.com>
2024-03-23 21:12:52 +0100

    Add files via upload

--- View/connexion.php
+++ View/connexion.php
@@ -1,24 +1,26 @@
 <html>
     <form method="post">
+        <fieldset>
+            <legend>Connexion</legend>
             <label for="login">Identifiant</label></br>
             <input type="text" name="login" id="login"></br>
-            <label for="password"> Mot de passe</label></br>
+            <label for="password">Mot de passe</label></br>
             <input type="password" name="password" id="password"></br>
-            <input type="submit" name="connection" id='connection'>  </br></br>
+            <input type="submit" name="connection" id='connection' value="Se connecter"></br>
+        </fieldset>
     </form>
-    </br>
 </html>
 <?php
 
-$testManager = new ManagerUtilisateur();
-
 if (isset($_POST['login']) && isset($_POST['password'])){
-    $testManager -> GetUser($_POST['login']);
-    $testManager -> checkLoginInfos($_POST['login'], $_POST['password']);
-    if ($testManager -> checkLoginInfos($_POST['login'], $_POST['password']) == true){
-        header("Location: ?id=".$testManager -> GetUser($_POST['login'])[0]['id']."&action=tableauDeBord");
-    } else if ($testManager -> checkLoginInfos($_POST['login'], $_POST['password']) == false){
-        echo "Erreur de connexion";
+    $utilisateur = new ManagerUtilisateur();
+    $utilisateur -> GetUser($_POST['login']);
+    $utilisateur -> checkLoginInfos($_POST['login'], $_POST['password']);
+    if ($utilisateur -> checkLoginInfos($_POST['login'], $_POST['password']) == true){
+        $_SESSION['id'] = $utilisateur -> GetUser($_POST['login'])[0]['id'];
+        header("Location: ?id=".$utilisateur -> GetUser($_POST['login'])[0]['id']."&action=tableauDeBord");
+    } else if ($utilisateur -> checkLoginInfos($_POST['login'], $_POST['password']) == false){
+        echo "-- Erreur de connexion --";
     }
 }
 
--- View/modifStatut.php
+++ View/modifStatut.php
@@ -1,23 +1,24 @@
-<?php 
-
-require_once('../Class/assistance.php');
-
-?>
 <html>
     <form method="post">
-            <label for="statut"> Pour changer le statut</label></br>
-            <input type="number" name="statut" id="statut" placeholder="entrer le statut (1,2,3)"></br>
-            <input type="number" name="idStatut" id="idStatut" placeholder="entrer l'id"></br>
-            <input type="submit" name="rechercher" id='rechercher' value='Rechercher'></br>
+        <label for="statut">Pour modifier le statut</label></br>
+        <input type="number" name="idStatut" id="idStatut" placeholder="id du problème"></br>
+        <select name="statut" id="statut">
+            <option value="">- Choix du statut -</option>
+            <option value=1>A faire</option>
+            <option value=2>En cours</option>
+            <option value=3>Terminé</option>
+        </select>
+        <input type="submit" name="rechercher" id='rechercher' value='Modifier'></br>
     </form>
 </html>
 <?php
 
 if (isset($_POST['statut']) && isset($_POST['idStatut'])){
-    $testManager = new ManagerAssistance();
-    $testManager -> updateStatut($_POST['idStatut'], $_POST['statut']);
-
-    echo "Statut changé";
+    $assistanceSatut = new ManagerAssistance();
+    $assistanceSatut -> updateStatut($_POST['idStatut'], $_POST['statut']);
+    if ($assistanceSatut -> updateStatut($_POST['idStatut'], $_POST['statut']) ==  true){
+        echo "-- Statut changé --";
+    }
 }
 
 ?>
