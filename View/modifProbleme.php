5e35c14b3c3d9cd5299f43f76db448a4aaeb9047
Noah-SIO <laplaze.noah@gmail.com>
2024-03-25 14:38:39 +0100

    suppr modifpbm

--- View/modifProbleme.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once("../Class/assistance.php");///////A supprimer quand constructeur en place 
-$bdsqll = new PDO("mysql:host=localhost;dbname=crm", 'root', '');//même chose
-$assistance = new ManagerAssistance($bdsqll);//même chose
-
-?>
