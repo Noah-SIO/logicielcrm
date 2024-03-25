7e02609af01ec8b5055ec32e1808bd64f3e181e7
Noah-SIO <laplaze.noah@gmail.com>
2024-03-25 16:24:13 +0100

    ajout test if type == 1

--- View/listeRappels.php
+++ View/listeRappels.php
@@ -6,8 +6,10 @@ $donneestableau = $rappel->getAlerteRappel($_SESSION['id']);
     echo "<h2>Vos Rappel et Alerte</h2></br>";
     if($donneestableau != NULL){
     for ($i = 0; $i < count($donneestableau); $i++) {
+        if($donneestableau[$i]->getType()==1){
         echo $donneestableau[$i]->getDateDebut(). " -- ".$donneestableau[$i]->getDateFin()."</br>";
         echo "<p>".$donneestableau[$i]->getSujet()."</p>";
         echo "<p>".$donneestableau[$i]->getContenu()."</p></br>";
+        }
     }}
 ?>
