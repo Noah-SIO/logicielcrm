1e666b67db4027b4f3916af50f8490c046812dfe
romain1021 <150360051+romain1021@users.noreply.github.com>
2024-03-26 09:53:27 +0100

    Statistiques.php Terminé
    normalement tout est ok :)

--- View/statistiques.php
+++ View/statistiques.php
@@ -1,5 +1,6 @@
 <?php
 require_once('../Class/assistance.php');
+require_once('../Class/fichecontact.php');
 $managerAssistance = new ManagerAssistance();
 $contact = new Contact();
 $StatOnContact = $contact->StatOnContact();
@@ -9,12 +10,12 @@ $statsTimeSolvedIssues = $managerAssistance->statsTimeSolvedIssues();
 
 <h2>Statistiques</h2>
 <h3>Nombre de problèmes signalé ces 10 derniers jours:</h3>
-<p><?php echo $statsNumberOfIssues ?></p>
+<p><?php echo $statsNumberOfIssues ?> problèmes</p>
 <h3> Temps de traitement des problèmes :</h3>
-<p><?php echo $statsTimeSolvedIssues ?></p>
+<p><?php echo $statsTimeSolvedIssues ?> jours</p>
 <h3>Nombre de clients :</h3>
 <p><?php echo $StatOnContact['nombreclient'] ?></p>
-<h3>Nombre de clients par jours :</h3>
+<h3>Nombre de contacts par jours :</h3>
 <p><?php echo $StatOnContact['nombreparjour'] ?></p>
-<h3>Nombre de clients par mois :</h3>
+<h3>Nombre de contacts par mois :</h3>
 <p><?php echo $StatOnContact['nombreparmois'] ?></p>
