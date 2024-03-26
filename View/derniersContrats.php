3f453d86bde83df3b32f23722dc6f967179530f4
romain1021 <150360051+romain1021@users.noreply.github.com>
2024-03-26 09:22:05 +0100

    ajout de la vue derniersContrats.php
    Fonctionel normalement

--- View/derniersContrats.php
+++ View/derniersContrats.php
@@ -1,10 +1,22 @@
 <?php
-$id_entreprise=0;
+$id_entreprise=0;//0 pour toute les entreprise 
 require_once("../class/fichier.php");
 $files = new ManagerFichier();
 $result = $files->GetFichierByClient($id_entreprise);
-$nbrAffichageProblème = 5;
-var_dump($result);
+echo '<table border="">';
+echo '<tr><th>ID</th><th>ID Entreprise</th><th>Nom</th><th>Type</th><th>Lien</th><th>Date</th></tr>';
+foreach ($result as $fichier) {
+    echo '<tr>';
+    echo '<td>' . $fichier->getId() . '</td>';
+    echo '<td>' . $fichier->getIdEntreprise() . '</td>';
+    echo '<td>' . $fichier->getNom() . '</td>';
+    echo '<td>' . $fichier->getType() . '</td>';
+    echo '<td>' . $fichier->getLienDoc() . '</td>';
+    echo '<td>' . $fichier->getDate() . '</td>';
+    echo '</tr>';
+}
+echo '</table>';
+
 
 
 ?>
