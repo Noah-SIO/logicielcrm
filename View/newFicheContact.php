c1e6c6b9a03d7fd4ebe0237a72343bdbd6928a78
romain1021 <150360051+romain1021@users.noreply.github.com>
2024-03-27 11:38:29 +0100

    Fonctionnel

--- View/newFicheContact.php
+++ View/newFicheContact.php
@@ -1,22 +1,32 @@
 <?php
+$iduser=1;// a modifier 
 require_once("../Class/fichecontact.php");///////A supprimer quand constructeur en place 
 $bdsqll = new PDO("mysql:host=localhost;dbname=crm", 'root', '');//même chose
 $ContactFiche = new Contact($bdsqll);//même chose
-echo"<h1>Formulaire Création de Fiche Contact</h1>";
-echo"<form method='post'>";
-    echo"<label for='identer'>L'id de l'Entreprise :</label></br></br>";
-    echo"<input type='number' name='identer' placeholder='ex : 1515' min='1' required></br></br>";
-    echo"<label for='moyen'>Le moyens de contact(Telephone Fixe 1, Smartphone 2, Email 3) :</label></br></br>";
-    echo"<input type='number' name='moyen' placeholder='0606060606 = 2' min='1' max='3' required></br></br>";
-    echo"<label for='dem'>La Demande :</label></br></br>";
-    echo"<input type='text' name='dem' placeholder='ex : probleme impression' required></br></br>";
-    echo"<label for='rep'>Et La Réponse :</label></br></br>";
-    echo"<textarea id='rep' name='rep' rows='7' cols='50' placeholder='Nous vous envoyons les données par mail' required></textarea></br></br>";
-    echo"<input type='submit' name='valider'class='button' value='Creer le Contact'/>";
+require_once('../class/entreprise.php');
+$entreprisemanager = new ManagerEntreprise();
+$listeEntreprise = $entreprisemanager->getAllEntreprise();?>
+<h1>Formulaire Création de Fiche Contact</h1>
+<form method='post'>
+     <label for="idEntreprise">Entreprise:</label>
+    <select name="idEntreprise" id="idEntreprise">
+        <?php foreach ($listeEntreprise as $entreprise) : ?>
+            <option value="<?php echo $entreprise->getId();?>">
+                <?php echo $entreprise->getSociete(); ?> (<?php echo $entreprise->getNom(); ?>)
+            </option>
+        <?php endforeach; ?>
+        </select><br><br>
+<label for='moyen'>Le moyen de contact (Telephone Fixe 1, Smartphone 2, Email 3) :</label><br>
+<input type='number' name='moyen' placeholder='ex : 0606060606 (2)' min='1' max='3' required width='100'><br><br>
+<label for='dem'>La demande :</label><br>
+<input type='text' name='dem' placeholder='ex : problème d’impression' required><br><br>
+<label for='rep'>Et la réponse :</label><br>
+<textarea id='rep' name='rep' rows='7' cols='50' placeholder='Nous vous envoyons les données par e-mail' required></textarea><br><br>
+<input type='submit' name='valider'class='button' value='Creer le Contact'>
+<?php
 echo"</form>";
 if (isset($_POST['valider'])) {
-    $iduser=$_SESSION['id'];
-    $newContact = new FicheContact( $iduser,$_POST['identer'],$_POST['moyen'],$_POST['dem'],$_POST['rep'],date('Y-m-d'));
+    $newContact = new FicheContact(null,$iduser,$_POST['idEntreprise'],date('Y-m-d'),$_POST['dem'],$_POST['rep'],$_POST['moyen']);
     $ContactFiche->createFicheContact($newContact);
     echo"<p>Fiche Contact Creer avec Succès !!!</p>";
 }
