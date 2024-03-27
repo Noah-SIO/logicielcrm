7351d109fd3bcf9d75ba40b3ec6d98ce9fe3870c
Yanisouspopo <tonperepointfr@gmail.com>
2024-03-27 11:43:37 +0100

    dernierContact.php

---
+++ View/dernierContact.php
@@ -0,0 +1,17 @@
+<?php
+include '../Class/fichecontact.php';
+$derniersProfils = $managerUtilisateur->returnAllUsers();
+$derniersTroisProfils = array_slice($derniersProfils, -3, 3);
+foreach ($derniersTroisProfils as $utilisateur) {
+    echo "ID : " . $utilisateur->getID() . "<br>";
+    echo "Nom : " . $utilisateur->getNom() . "<br>";
+    echo "Prénom : " . $utilisateur->getPrenom() . "<br>";
+    echo "Identifiant : " . $utilisateur->getIdentifiant() . "<br>";
+    echo "Profil : " . $utilisateur->getProfil() . "<br>";
+    echo "Droits : " . $utilisateur->getProfil() . "<br>";
+    echo "<hr>";
+}
+?>
+
+
+# EN COURS A MODIF (rapide)
