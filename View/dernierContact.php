9109c57dcbff2c0943cdbd4ec387791dc782b8d3
romain1021 <150360051+romain1021@users.noreply.github.com>
2024-03-28 08:26:11 +0100

    modifs

---
+++ View/dernierContact.php
@@ -0,0 +1,15 @@
+<?php
+$nbrAAfficher = 5;
+require_once("../Class/fichecontact.php");
+$contactManager = new Contact();
+$listeContacts = $contactManager->getContactHistorique(4,$nbrAAfficher);
+foreach ($listeContacts as $contact) {
+    echo "<ul>";
+    echo "<li><strong>Identifiant Compte:</strong> " . $contact->getIdCompte() . "</li>";
+    echo "<li><strong>Identifiant Entreprise:</strong> " . $contact->getIdEntreprise() . "</li>";
+    echo "<li><strong>Date:</strong> " . $contact->getDate() . "</li>";
+    echo "<li><strong>Moyen de Contact:</strong> " . $contact->getMoyenDeContact() . "</li>";
+    echo "<li><strong>Demande:</strong> " . $contact->getDemande() . "</li>";
+    echo "<li><strong>Réponse:</strong> " . $contact->getReponse() . "</li>";
+    echo "</ul>";
+}
