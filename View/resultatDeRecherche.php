b4146d0aa92b4273b7e352293be65b702c7ab490
romain1021 <150360051+romain1021@users.noreply.github.com>
2024-04-02 09:15:17 +0200

    ...

--- View/resultatDeRecherche.php
+++ View/resultatDeRecherche.php
@@ -1,7 +1,6 @@
 <?php
 $statutUtilisateur = $_SESSION['droit'];//a modifier
-
-$recherche = $_SESSION['recherche'];// a modifier 
+$recherche = $_GET['recherche'];// a modifier un poste
 
 //entreprise
 $managerEntreprise = new ManagerEntreprise();
@@ -31,8 +30,7 @@ if(!empty($listeEntreprise)){
     echo "<p>Resultat de recherche Entreprise</p>";
     foreach($listeEntreprise as $entreprise){
         echo "Nom: ".$entreprise->getNom()." - Prenom: ".$entreprise->getPrenom()." - Societe: ".$entreprise->getSociete()." - Poste: ".$entreprise->getPoste()."</p>";
-        // echo "<a href='infoEntreprise.php?id=".$entreprise->getId()."'><button>Voir fiche</button></a>";
-        echo "<a href='?action=infoEntreprise&id=".$entreprise->getId()."'><button>Voir fiche</button></a></br>";
+        echo "<a href='infoEntreprise.php?id=".$entreprise->getId()."'><button>Voir fiche</button></a>";
 
     }
 }
@@ -49,8 +47,7 @@ if(!empty($listeUtilisateur)){
     echo "<p>Resultat de recherche Utilisateur</p>";
     foreach($listeUtilisateur as $utilisateur){
         echo "<p>ID: ".$utilisateur->getId()." - Nom: ".$utilisateur->getNom()." - Prenom: ".$utilisateur->getPrenom()." - Identifiant: ".$utilisateur->getIdentifiant()." - Profil: ".$utilisateur->getProfil()."</p>";
-        // echo "<a href='ficheProfil.php?id=".$utilisateur->getId()."'><button>Voir fiche</button></a>";
-        echo "<a href='?action=ficheProfil&id=".$utilisateur->getId()."'><button>Voir fiche</button></a></br>";
+        echo "<a href='ficheProfil.php?id=".$utilisateur->getId()."'><button>Voir fiche</button></a>";
     }
 }
 else{
@@ -65,9 +62,7 @@ if(!empty($fichiers)){
     echo "<p>Resultat de recherche Fichier</p>";
     foreach($fichiers as $fichier){
         echo"Nom: ".$fichier->getNom()." - Type: ".$fichier->getType()." - Date: ".$fichier->getDate()."</p>";
-        // echo "<a href='download.php?file=".$fichier->getLienDoc()."'><button>télécharger le fichier</button></a>";
-        echo "<a href='?action=download&file=".$fichier->getLienDoc()."'><button>télécharger le fichier</button></a>";
-
+        echo "<a href='download.php?file=".$fichier->getLienDoc()."'><button>télécharger le fichier</button></a>";
     }
 }
 else{
@@ -81,8 +76,7 @@ if(!empty($contact)){
     echo "<p>Resultat de recherche Contact</p>";
     foreach($contact as $contact){
         echo "Date: " . $contact->getDate() . " - Demande: " ,$contact->getDemande() . " - Reponse: " . htmlspecialchars($contact->getReponse(), ENT_QUOTES, 'UTF-8') . ".</p>";
-        // echo '<a href="FicheContact.php?id='.$contact->getId(). '"><button>Voir fiche</button></a>';
-        echo "<a href='?action=ficheContact&id=".$contact->getId()."'><button>Voir fiche</button></a></br>";
+        echo '<a href="FicheContact.php?id='.$contact->getId(). '"><button>Voir fiche</button></a>';
     }
 }//$id,$idCompte, $idEntreprise, $date, $demande, $reponse, $moyenDeContact
 else{
