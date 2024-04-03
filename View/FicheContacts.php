f4a62ac511cdaebf81a41e4228e7618073aa20a5
romain1021 <150360051+romain1021@users.noreply.github.com>
2024-04-03 11:21:06 +0200

    Update FicheContacts.php

--- View/FicheContacts.php
+++ View/FicheContacts.php
@@ -1,11 +1,12 @@
 <?php
 $idFicheContact = $_GET['id']; // a modifier 
 $contactManager = new Contact();
+$utilisateur = new ManagerUtilisateur();
 $listeContacts = $contactManager->getContactByID($idFicheContact);
 echo"<h1>Fiche Contact</h1>";
 foreach ($listeContacts as $contact) {
     echo "<ul>";
-    echo "<li><strong>Identifiant Compte:</strong> " . $contact->getIdCompte() . "</li>";
+    echo "<li><strong>Conseiller :</strong> " . $utilisateur->GetUserById($contact->getIdCompte())[0]['nom'].", ". $utilisateur->GetUserById($contact->getIdCompte())[0]['prenom'] . "</li>";
     echo "<li><strong>Identifiant Entreprise:</strong> " . $contact->getIdEntreprise() . "</li>";
     echo "<li><strong>Date:</strong> " . $contact->getDate() . "</li>";
     echo "<li><strong>Moyen de Contact:</strong> " . $contact->getMoyenDeContact() . "</li>";
