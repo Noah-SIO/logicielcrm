019e36e031d370a7ad5cc57e8fc802f4085c0798
romain1021 <150360051+romain1021@users.noreply.github.com>
2024-04-03 10:34:21 +0200

    modifs

--- View/ficheProfil.php
+++ View/ficheProfil.php
@@ -5,11 +5,10 @@ if(isset($_GET['id'])) {
     $utilisateur = $managerUtilisateur->GetUserById($idFicheProfilAChercher);
     if ($utilisateur != NULL) {
         echo"<h1>Fiche Profil</h1>";
-        echo "ID : " . $utilisateur[0]['id'] . "<br>";
-        echo "Nom : " . $utilisateur[0]['nom'] . "<br>";
-        echo "Prénom : " . $utilisateur[0]['prenom'] . "<br>";
-        echo "Identifiant : " . $utilisateur[0]['identifiant'] . "<br>";
-        echo "Droit : " . $poste[$utilisateur[0]['droit']] . "<br>";
+        echo "<h3>Nom : </h3>" . $utilisateur[0]['nom'] . "<br>";
+        echo "<h3>Prénom : </h3>" . $utilisateur[0]['prenom'] . "<br>";
+        echo "<h3>Identifiant : </h3>" . $utilisateur[0]['identifiant'] . "<br>";
+        echo "<h3>Droit : </h3>" . $poste[$utilisateur[0]['droit']] . "<br>";
     } else {
         echo "Cette id n'est assigné a aucune fiche.";
     }
