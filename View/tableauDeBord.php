97845ad5450ff503ef1c940eb1bfec5d12e79f28
Noah-SIO <laplaze.noah@gmail.com>
2024-04-04 09:37:09 +0200

    modif droit attachedocument

--- View/tableauDeBord.php
+++ View/tableauDeBord.php
@@ -25,9 +25,6 @@ if ($_SESSION['droit'] == 2){
 }
 
 if ($_SESSION['droit'] == 3){
-    echo "<div class='attacheDocument'>";
-    require('View/attacheDocument.php');
-    echo "</div>";
     echo "<div class='listeFicheContact'>";
     require('View/listeFicheContact.php');
     echo "</div>";
