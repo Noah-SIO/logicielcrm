560c516309aaca8815641616ddc04656957a4086
Noah-SIO <laplaze.noah@gmail.com>
2024-04-04 09:21:50 +0200

    modif visuel en strong

--- View/resultatDeRecherche.php
+++ View/resultatDeRecherche.php
@@ -27,15 +27,15 @@ $contact = $managerContact->getContactByID((int)$recherche);
 
 if($statutUtilisateur == 1 or $statutUtilisateur == 4 or $statutUtilisateur == 5){
 if(!empty($listeEntreprise)){
-    echo "<p>Resultat de recherche Entreprise</p>";
+    echo "<strong><p>Resultat de recherche Entreprise</p></strong>";
     foreach($listeEntreprise as $entreprise){
         echo "Nom: ".$entreprise->getNom()." - Prenom: ".$entreprise->getPrenom()." - Societe: ".$entreprise->getSociete()." - Poste: ".$entreprise->getPoste()."</p>";
-        echo "<a href='index.php?action=ficheEntreprise&id=".$entreprise->getId()."'><button>Voir fiche</button></a>";
+        echo "<a href='index.php?action=ficheEntreprise&id=".$entreprise->getId()."'><button>Voir fiche</button></a></br></br>";
 
     }
 }
 else{
-    echo "<p>Aucun entreprise trouve</p>";
+    echo "<strong><p>Aucun entreprise trouve</p></strong>";
 }
 }
 
@@ -44,14 +44,14 @@ else{
 
 if($statutUtilisateur == 1 or $statutUtilisateur == 5 or $statutUtilisateur == 6){
 if(!empty($listeUtilisateur)){
-    echo "<p>Resultat de recherche Utilisateur</p>";
+    echo "<strong><p>Resultat de recherche Utilisateur</p></strong>";
     foreach($listeUtilisateur as $utilisateur){
         echo "Nom: ".$utilisateur->getNom()." - Prenom: ".$utilisateur->getPrenom()." - Identifiant: ".$utilisateur->getIdentifiant()." - Profil: ".$utilisateur->getProfil()."</p>";
         echo "<a href='index.php?action=ficheProfil&id=".$utilisateur->getId()."'><button>Voir fiche</button></a></br></br>";
     }
 }
 else{
-    echo "<p>Aucun utilisateur trouve</p>";
+    echo "<strong><p>Aucun utilisateur trouve</p></strong>";
 }
 }
 
@@ -59,28 +59,28 @@ else{
 //recherche de fichier
 if($statutUtilisateur == 1 or $statutUtilisateur == 4 or $statutUtilisateur == 5 or $statutUtilisateur == 6){
 if(!empty($fichiers)){
-    echo "<p>Resultat de recherche Fichier</p>";
+    echo "<strong><p>Resultat de recherche Fichier</p></strong>";
     foreach($fichiers as $fichier){
         echo "Nom: ".$fichier->getNom()." - Type: ".$fichier->getType()." - Date: ".$fichier->getDate()."<br>";
         echo "<a href='view/download.php?file=../".$fichier->getLienDoc()."'><button>télécharger ". $fichier->getNom()." </button></a>"."<br><br>";
     }
 }
 else{
-    echo "<p>Aucun fichier trouve</p>";
+    echo "<strong><p>Aucun fichier trouve</p></strong>";
 }
 }
 
 //fiche de contact
 if($statutUtilisateur == 1 or $statutUtilisateur == 2 or $statutUtilisateur == 3 or $statutUtilisateur == 5 or $statutUtilisateur == 6){
 if(!empty($contact)){
-    echo "<p>Resultat de recherche Contact</p>";
+    echo "<strong><p>Resultat de recherche Contact</p></strong>";
     foreach($contact as $contact){
         echo "Date: " . $contact->getDate() . " - Demande: " ,$contact->getDemande() . " - Reponse: " .$contact->getReponse(). ".</p>";
         echo '<a href="index.php?action=ficheContact&id='.$contact->getId(). '"><button>Voir fiche</button></a>';
         }
 }//$id,$idCompte, $idEntreprise, $date, $demande, $reponse, $moyenDeContact
 else{
-    echo "<p>Aucun contact trouve</p>";
+    echo "<strong><p>Aucun contact trouve</p></strong>";
 }
 }
 
