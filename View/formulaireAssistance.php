6eae63a5fd1479b60971b51c2baef5be02f0f7b5
Noah-SIO <laplaze.noah@gmail.com>
2024-04-05 09:35:37 +0200

    modif

--- View/formulaireAssistance.php
+++ View/formulaireAssistance.php
@@ -6,9 +6,8 @@ if(isset($_POST['idrespinfo'])){
     $assistance->registerIssueNoConnect($_POST['idrespinfo'],$_POST['nom'],$_POST['suj'],$_POST['mess']);
     echo"<strong><p>----------------Votre problème à bien été transmis à nos Administrateur---------------</p></strong></br></br>";
     }
-echo "<form method='post'>";
-echo "<label for='idrespinfo'>Entrer un Responsable Informatique :</label></br></br>";
-$bd = new PDO("mysql:host=localhost;dbname=crm", 'root', '');
+    echo"<label for='idrespinfo'>Entrer un Responsable Informatique :</label></br></br>";
+    $bd = new PDO("mysql:host=localhost;dbname=crm", 'root', '');
     $s = 'SELECT id, nom, prenom, droit FROM utilisateur WHERE droit = 5';
     $r = $bd -> query($s);
     $donnees = $r -> fetchAll(PDO::FETCH_ASSOC);
@@ -17,19 +16,15 @@ $bd = new PDO("mysql:host=localhost;dbname=crm", 'root', '');
         <label for='".$donnees[$i]['prenom']."'>".$donnees[$i]['nom']." ".$donnees[$i]['prenom']."</label>
         </div></br>";
     }
-echo "<label for='nom'>Entrer votre Nom :</label></br></br>";
-echo "<input type='text' name='nom' placeholder='ex : Latour' required></br></br>";
-echo "<label for='telephone'>Entrer votre Numéro de téléphone :</label></br></br>";
-echo "<input type='tel' name='telephone' placeholder='ex : 0123456789' required></br></br>";
-echo "<label for='email'>Entrer votre Email :</label></br></br>"; 
-echo "<input type='email' name='email' placeholder='ex : test@test.com' required></br></br>";
-echo "<label for='suj'>Entrer Le sujet de votre Problème :</label></br></br>";
-echo "<input type='text' name='suj' placeholder='Un problème de MDP' required></br></br>";
-echo "<label for='mess'>Et votre Message :</label></br></br>";
-echo "<textarea id='mess' name='mess' rows='7' cols='50' placeholder='salut c est jêrome du service comptable j ai un problème de mot de passe...' required></textarea></br></br>";
-echo "<input type='submit' name='valider' class='button' value='Envoyer au Service Informatique'/>";
-echo "</form>";
-echo "<form action=''>";
-echo "<input type='submit' name='valider' class='button' value='Retourner à la page de connexion'/>";
-echo "</form>";
+    echo"<label for='nom'>Entrer votre Nom :</label></br></br>";
+    echo"<input type='text' name='nom' placeholder='ex : Latour' required></br></br>";
+    echo"<label for='suj'>Entrer Le sujet de votre Problème :</label></br></br>";
+    echo"<input type='text' name='suj' placeholder='Un problème de MDP' required></br></br>";
+    echo"<label for='mess'>Et votre Message :</label></br></br>";
+    echo"<textarea id='mess' name='mess' rows='7' cols='50' placeholder='salut c est jêrome du service comptable j ai un problème de mot de passe...' required></textarea></br></br>";
+    echo"<input type='submit' name='valider'class='button' value='Envoyer au Service Informatique'/>";
+echo"</form>"; 
+echo"<form action=''>";
+echo"<input type='submit' name='valider' class='button' value='Retourner page connexion'/>";
+echo"</form>";
 ?>
