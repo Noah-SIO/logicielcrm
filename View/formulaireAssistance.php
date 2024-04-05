56fc8e5c235dd518596d582cb36e4f6c03d6a01e
Noah-SIO <laplaze.noah@gmail.com>
2024-04-05 09:25:46 +0200

    rajout suite a ecrasement des fichier

--- View/formulaireAssistance.php
+++ View/formulaireAssistance.php
@@ -1,10 +1,22 @@
 <?php
 $assistance = new ManagerAssistance();
-echo "<h1>Formulaire De Contact Services Informatique</h1>";
-echo "<p>Pour les problèmes de mot de passe, tapez 1, pour la création de compte, tapez 2.</p></br></br></br>";
+echo"<h1>Formulaire De Contact Services Informatique</h1></br>";
+echo"<form method='post'>";
+if(isset($_POST['idrespinfo'])){
+    $assistance->registerIssueNoConnect($_POST['idrespinfo'],$_POST['nom'],$_POST['suj'],$_POST['mess']);
+    echo"<strong><p>----------------Votre problème à bien été transmis à nos Administrateur---------------</p></strong></br></br>";
+    }
 echo "<form method='post'>";
 echo "<label for='idrespinfo'>Entrer un Responsable Informatique :</label></br></br>";
-echo "<input type='text' name='idrespinfo' placeholder='1 ou 2...' required></br></br>";
+$bd = new PDO("mysql:host=localhost;dbname=crm", 'root', '');
+    $s = 'SELECT id, nom, prenom, droit FROM utilisateur WHERE droit = 5';
+    $r = $bd -> query($s);
+    $donnees = $r -> fetchAll(PDO::FETCH_ASSOC);
+    for ($i = 0; $i < count($donnees); $i++) {
+        echo "<div><input type='radio' id='".$donnees[$i]['prenom']."' name='idrespinfo' value=".$donnees[$i]['id']." />
+        <label for='".$donnees[$i]['prenom']."'>".$donnees[$i]['nom']." ".$donnees[$i]['prenom']."</label>
+        </div></br>";
+    }
 echo "<label for='nom'>Entrer votre Nom :</label></br></br>";
 echo "<input type='text' name='nom' placeholder='ex : Latour' required></br></br>";
 echo "<label for='telephone'>Entrer votre Numéro de téléphone :</label></br></br>";
@@ -17,10 +29,6 @@ echo "<label for='mess'>Et votre Message :</label></br></br>";
 echo "<textarea id='mess' name='mess' rows='7' cols='50' placeholder='salut c est jêrome du service comptable j ai un problème de mot de passe...' required></textarea></br></br>";
 echo "<input type='submit' name='valider' class='button' value='Envoyer au Service Informatique'/>";
 echo "</form>";
-if(isset($_POST['idrespinfo'])){
-    $assistance->registerIssueNoConnect($_POST['idrespinfo'], $_POST['suj'], $_POST['suj'], $_POST['mess'], $_POST['telephone'], $_POST['email']);
-    echo "Votre problème a bien été transmis à nos Administrateurs";
-}
 echo "<form action=''>";
 echo "<input type='submit' name='valider' class='button' value='Retourner à la page de connexion'/>";
 echo "</form>";
--- View/formulaireAssistanceCo.php
+++ View/formulaireAssistanceCo.php
@@ -1,9 +1,4 @@
 <?php
-if(isset($_POST['idrespinfo'])){
-    $assistance->registerIssue($_POST['idrespinfo'], $_SESSION['id'], $_POST['suj'], $_POST['mess']);
-    echo"<h3>Votre problème à bien été transmis à nos Administrateur</h3>";
-
-}
 $assistance = new ManagerAssistance();
 echo"<h1>Formulaire De Contact Services Informatique</h1></br>";
 if(isset($_POST['idrespinfo'])){
@@ -28,11 +23,4 @@ echo"<label for='idrespinfo'>Entrer un Responsable Informatique :</label></br></
     echo"<textarea id='mess' name='mess' rows='7' cols='50' placeholder='salut c est jêrome du service comptable j ai un problème de mot de passe...' required></textarea></br></br>";
     echo"<input type='submit' name='valider'class='button' value='Envoyer au Service Informatique'/></br>";
     echo"</form>";
-
-if(isset($_POST['idrespinfo'])){
-    $assistance->registerIssue($_POST['idrespinfo'], $_SESSION['id'], $_POST['suj'], $_POST['mess']);
-    echo"Votre problème à bien été transmis à nos Administrateur, veuillez maintenant patienté.";
-
-}
-
 ?>
