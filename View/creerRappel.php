61e77ca7fc8f252e8bd47184e4556b643ba24eeb
romain1021 <150360051+romain1021@users.noreply.github.com>
2024-04-09 08:50:53 +0200

    Reaparation des problèmes de mon commit

--- View/creerRappel.php
+++ View/creerRappel.php
@@ -3,29 +3,27 @@ $rappel = new ManagerRappelAlerte();//même chose
 $user = new ManagerUtilisateur();//même chose
 $donneestableau = $user->SearchUserByType('%','ALL');
 echo"<h1>Formulaire Création de Rappel</h1>";
-echo"<p>Ce service sert aux employés de l'entreprise à ne pas oublié les taches qu'il auront à effectuer en créant un rappel qu'il recevront d'ici quelques jour.</p></br>";
-echo"<form method='post'>";
-    echo"<label for=''>Choix destinataire :</label></br></br>";
-    echo"<input type='checkbox' id='sois' name='destinataire' value=".$_SESSION['id']."/>";
-    echo"<label for='sois'>Sois-même</label>";
-    echo"</div><div>";
-    echo"</br><select name='commercial' id='commercial-select'>";
-        echo"<option value=''>--Choisissez un Destinataire--</option>";
-        if($donneestableau != NULL){
-            for ($i = 0; $i < count($donneestableau); $i++) {
-                echo"<option value='".$donneestableau[$i]->getNom()."'>".$donneestableau[$i]->getNom()."</option>";
-                }
-            }
-    echo"</select>";
-    echo"</div> </br>";
-    echo"<label for='nom'>Le sujet:</label></br></br>";
-    echo"<input type='text' name='suj' placeholder='ex : Rappeler Mr Latour' required></br></br>";
-    echo"<label for='nom'>La Date de Fin de Rappel :</label></br></br>";
-    echo "<input type='date' id='date' name='date' required/></br></br>";
-    echo"<label for='mess'>Et votre Message :</label></br></br>";
-    echo"<textarea id='mess' name='mess' rows='7' cols='50' placeholder='Rappeler Mr Latour le 28/03/2024 a 9h pour son probleme de MDP' required></textarea></br></br>";
-    echo"<input type='submit' name='valider' class='button' value='Créer'/>";
-echo"</form>";
+echo "<p>Ce service permet aux employés de l'entreprise de ne pas oublier les tâches qu'ils auront à effectuer en créant un rappel qu'ils recevront d'ici quelques jours.</p></br>";
+echo "<form method='post'>";
+echo "<label for=''>Choix destinataire :</label><br/><br/>";
+echo "<input type='checkbox' id='sois' name='destinataire' value='" . $_SESSION['id'] . "'/>";
+echo "<label for='sois'>Sois-même</label><br/>";
+echo "<div><select name='commercial' id='commercial-select'>";
+echo "<option value=''>--Choisissez un destinataire--</option>";
+if ($donneestableau != null) {
+    foreach ($donneestableau as $utilisateur) {
+        echo "<option value='" . $utilisateur->getNom() . "'>" . $utilisateur->getNom() . "</option>";
+    }
+}
+echo "</select></div><br/>";
+echo "<label for='nom'>Le sujet :</label><br/><br/>";
+echo "<input type='text' name='suj' placeholder='ex : Rappeler M. Latour' required><br/><br/>";
+echo "<label for='nom'>La date de fin de rappel :</label><br/><br/>";
+echo "<input type='date' id='date' name='date' required><br/><br/>";
+echo "<label for='mess'>Et votre message :</label><br/><br/>";
+echo "<textarea id='mess' name='mess' rows='7' cols='50' placeholder='Rappeler M. Latour le 28/03/2024 à 9h pour son problème de MDP' required></textarea><br/><br/>";
+echo "<input type='submit' name='valider' class='button' value='Créer'/>";
+echo "</form>";
 
 if (isset($_POST['valider'])) {
     //$tableaudonnees = $user->GetUser($_POST['identif']);
@@ -40,6 +38,6 @@ if (isset($_POST['valider'])) {
         $rappelAlerte = new RappelAlerte( NULL,date("Y-m-d"),$_POST['date'],1,$_SESSION['id'],$idexpediteur,$_POST['suj'],$_POST['mess'],1);
         }
     $rappel->sendAlerteRappel($rappelAlerte);
-    echo"<strong><p>----------------Rappel créé avec Succès !!!---------------</p></strong></br></br>";
+    echo"<strong><p>----------------Rappel créé avec Succès !!!---------------</p></strong><br/><br>/";
 }
 ?>
--- View/listeRappels.php
+++ View/listeRappels.php
@@ -5,20 +5,10 @@ $donneestableau = $rappel->getAlerteRappel($_SESSION['id']);
     echo "<h2>Vos Rappels et Alertes</h2></br>";
     if($donneestableau != NULL){
     for ($i = 0; $i < count($donneestableau); $i++) {
-        $_GET['idAlerte'] = $donneestableau[$i]->getId();
-
-        echo "<ul>";
-        echo "<li> <b>Statut :</b> ".$alerte[$donneestableau[$i]->getStatut()]. "";
-        if ($donneestableau[$i]->getStatut() == 1){
-            $_GET['statutAlerte'] = $donneestableau[$i]->getStatut();
-            echo " <a href='?action=modifierAlerteRappel&idAlerte=".$_GET['idAlerte']."&statutAlerte=".$_GET['statutAlerte']."''><button>Terminer</button></a>";
-        }
-        echo "</li> <li> <b>Début :</b> ".$donneestableau[$i]->getDateDebut(). " </li>";
-        echo "<li> <b>Fin :</b> ".$donneestableau[$i]->getDateFin()."</li>";
-        echo "<li> <b>Sujet :</b> ".$donneestableau[$i]->getSujet()."</li>";
-        echo "<li> <b>Contenu :</b> ".$donneestableau[$i]->getContenu();
-        echo "</li>
-            </ul>";
+        echo $donneestableau[$i]->getDateDebut(). " -- ".$donneestableau[$i]->getDateFin()."</br>";
+        echo "<p>".$donneestableau[$i]->getSujet()."</p>";
+        echo "<p>".$donneestableau[$i]->getContenu()."</p></br>";
+        echo "<hr>";
         }
     }
 ?>
