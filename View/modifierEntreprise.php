89d500df26e2b00ab8f60e7a909302f3ba6fc067
ciraam <150357561+ciraam@users.noreply.github.com>
2024-04-12 10:42:08 +0200

    Add files via upload

--- View/modifierEntreprise.php
+++ View/modifierEntreprise.php
@@ -16,14 +16,22 @@ if (isset($_POST['modifier'])) {
     $prenom = $_POST['prenom'];
     $societe = $_POST['societe'];
     $poste = $_POST['poste'];
-    $id_commercial = $_POST['commercial'];
-    $date = $_POST['date'];
-    $modifEntreprie = new Entreprise(NULL,$nom, $prenom, $societe,$poste, $id_commercial, $date);
-    $modifEntreprie->setId($idEntreprise);
-    $entrepriseManager->ModifClient($modifEntreprie);
-    echo"<h3>Entreprise Modifier avec Succès !!!</h3>";
-    //echo "<script>setTimeout(function(){location.reload(); },3000);</script>";
     
+    $date = $_POST['date'];
+    if ($_SESSION['droit'] == 3){
+        $modifEntreprie = new Entreprise(NULL,$nom, $prenom, $societe,$poste, $_SESSION['id'], $date);
+        $modifEntreprie->setId($idEntreprise);
+        $entrepriseManager->ModifClient($modifEntreprie);
+        echo"<h3>Entreprise Modifier avec Succès !!!</h3>";
+        //echo "<script>setTimeout(function(){location.reload(); },3000);</script>";
+    } else {
+        $id_commercial = $_POST['commercial'];
+        $modifEntreprie = new Entreprise(NULL,$nom, $prenom, $societe,$poste, $id_commercial, $date);
+        $modifEntreprie->setId($idEntreprise);
+        $entrepriseManager->ModifClient($modifEntreprie);
+        echo"<h3>Entreprise Modifier avec Succès !!!</h3>";
+        //echo "<script>setTimeout(function(){location.reload(); },3000);</script>";
+    }
 }
 
 
@@ -39,14 +47,21 @@ if (isset($_POST['modifier'])) {
     <input type="text" name="societe" value="<?php echo $societe; ?>"><br>
     <label for="poste">Poste:</label>
     <input type="text" name="poste" value="<?php echo $infosEntreprise->getPoste(); ?>"><br>
-    <label for="commercial">Commercial :</label><br>
-    <select name="commercial" id="commercial">
-        <?php foreach ($commercial as $com) : ?>
-            <option value="<?php echo $com->getId();?>" <?php if ($com->getId() == $id_commercial) echo 'selected'; ?>>
-                <?php echo $com->getNom() . " " . $com->getPrenom(); ?>
-            </option>
-        <?php endforeach; ?>
-    </select><br><br>
+    
+    <?php
+    if ($_SESSION['droit'] == 1){
+        echo '<label for="commercial">Commercial :</label><br>
+            <select name="commercial" id="commercial">';
+            foreach ($commercial as $com){
+                if ($com ->getProfil() == 3){
+                    echo '<option value="'.$com->getId().'">'.$com->getNom() . " " . $com->getPrenom().'</option>';
+                }
+            }
+        echo '</select><br><br>';
+    }  else if ($_SESSION['droit'] == 3){
+
+    }
+    ?>
     <label for="date">Date:</label>
     <input type="date" name="date" value="<?php echo $date; ?>"><br>
     <input type="submit" name="modifier" value="Modifier" class="button">
